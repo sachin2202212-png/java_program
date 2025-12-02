4298c09fce493662de0547396906f8109737d003
sachin2202212-png <sachin.2202212@srec.ac.in>
2025-12-02 09:32:01 +0530

    Add files via upload

---
+++ round_to_nearest_greater_Integer.java.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        float num1=sc.nextFloat();
+        int num2=(int)Math.ceil(num1);
+        System.out.println(num2);
+    }
+}
