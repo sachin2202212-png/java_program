fc5ba12ed98412a9a983e0779524a7134bcfd3ee
sachin2202212-png <sachin.2202212@srec.ac.in>
2025-12-02 09:46:55 +0530

    Add files via upload

---
+++ ninja_clan_difference.java.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int N=sc.nextInt();
+        int M=sc.nextInt();
+        if(M>N){
+            System.out.println(M-N);
+        }
+        else
+        {
+            System.out.println(N-M);
+        }
+    }
+}
