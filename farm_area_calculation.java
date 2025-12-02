429eaafbf44b41d8cea7d2c7d17f00d5e27d5f41
sachin2202212-png <sachin.2202212@srec.ac.in>
2025-12-02 11:05:15 +0530

    Add files via upload

---
+++ farm_area_calculation.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    float L=sc.nextFloat();
+    float B=sc.nextFloat();
+    float tot=L*B;
+    System.out.printf("%.5f",tot);
+    }
+}
