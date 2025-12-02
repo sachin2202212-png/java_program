4bbd8a1838069a0d4acfe14fa02c5fe052cd679b
sachin2202212-png <sachin.2202212@srec.ac.in>
2025-12-02 09:39:29 +0530

    Add files via upload

---
+++ print_factors_of_a_number.java.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        for(int i=1;i<=N/2;i++)
+        {
+            if(N%i==0){
+                System.out.print(i+" ");
+            }
+        }
+        System.out.print(N);
+    }
+}
